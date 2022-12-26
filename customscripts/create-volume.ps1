009b3684920932922949c0f91a043baeb0cbf94e
Takeshi Katano <takatano@outlook.com>
2022-12-27 02:01:11 +0900

    Add custom script

---
+++ customscripts/create-volume.ps1
@@ -0,0 +1,47 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string] $DriveLetter,
+
+    [Parameter(Mandatory = $false)]
+    [string] $VolumeLabel = 'HCI Lab Data',
+
+    [Parameter(Mandatory = $false)]
+    [string] $StoragePoolName = 'hcisandboxpool'
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+
+# Create a storage pool.
+
+New-StoragePool -FriendlyName $StoragePoolName -StorageSubSystemFriendlyName '*storage*' -PhysicalDisks (Get-PhysicalDisk -CanPool $true)
+
+if ((Get-StoragePool -FriendlyName $StoragePoolName -ErrorAction SilentlyContinue).OperationalStatus -ne 'OK') {
+    throw 'Storage pool creation failed.'
+}
+
+# Create a volume.
+
+New-Volume -StoragePoolFriendlyName $StoragePoolName -FileSystem NTFS -AllocationUnitSize 64KB -ResiliencySettingName Simple -UseMaximumSize -DriveLetter $DriveLetter -FriendlyName $VolumeLabel
+
+if ((Get-Volume -DriveLetter $DriveLetter -ErrorAction SilentlyContinue).OperationalStatus -ne 'OK') {
+    throw 'Volume creation failed.'
+}
+
+# Set Defender exclusions.
+
+$exclusionPath = $DriveLetter + ':\'
+
+Add-MpPreference -ExclusionPath $exclusionPath
+
+if ((Get-MpPreference).ExclusionPath -notcontains $exclusionPath) {
+    throw 'Defender exclusion setting failed.'
+}
+
+# Create the folder structure on the volume.
+
+New-Item -ItemType Directory -Path ([IO.Path]::Combine($DriveLetter, 'temp')) -Force
+New-Item -ItemType Directory -Path ([IO.Path]::Combine($DriveLetter, 'iso')) -Force
+New-Item -ItemType Directory -Path ([IO.Path]::Combine($DriveLetter, 'vhd')) -Force
+New-Item -ItemType Directory -Path ([IO.Path]::Combine($DriveLetter, 'vm')) -Force
