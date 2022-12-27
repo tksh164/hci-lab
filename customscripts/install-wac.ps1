375b25e55a82eb1ad57e9d389bb9d5c9a4dd3ca8
Takeshi Katano <takatano@outlook.com>
2022-12-27 14:36:04 +0900

    Add WAC installation custom script

---
+++ customscripts/install-wac.ps1
@@ -0,0 +1,29 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [string] $ConfigParametersFile = '.\config-parameters.json'
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+
+$logFolderPath = 'C:\Temp'
+New-Item -ItemType Directory -Path $logFolderPath -Force
+Start-Transcript -OutputDirectory $logFolderPath
+
+Import-Module -Name '.\common.psm1'
+
+$configParams = GetConfigParametersFromJsonFile -FilePath $ConfigParametersFile
+
+# Create the download folder if it does not exist.
+New-Item -ItemType Directory -Path $configParams.tempFolder -Force
+
+# Download the Windows Admin Center installer.
+$wacMsiFilePath = [IO.Path]::Combine($configParams.tempFolder, 'WindowsAdminCenter.msi')
+Start-BitsTransfer -Source 'https://aka.ms/WACDownload' -Destination $wacMsiFilePath
+
+# Install Windows Admin Center.
+$msiArgs = '/i', ('"{0}"' -f $wacMsiFilePath), '/qn', '/L*v', 'log.txt', 'SME_PORT=443', 'SSL_CERTIFICATE_OPTION=generate'
+Start-Process -FilePath 'msiexec.exe' -ArgumentList $msiArgs -Wait
+
+Stop-Transcript
