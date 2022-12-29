d2b29c4407b61c6e45ec06aadf93fd4167bca88c
Takeshi Katano <takatano@outlook.com>
2022-12-29 22:51:40 +0900

    Add custom script

---
+++ customscripts/configure-hyperv.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+param ()
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+
+Import-Module -Name '.\common.psm1'
+
+$configParams = GetConfigParameters
+Start-Transcript -OutputDirectory $configParams.folderPath.transcript
+$configParams | ConvertTo-Json -Depth 16
+
+# Set Hyper-V host settings.
+$params = @{
+    VirtualMachinePath        = $configParams.folderPath.vm
+    VirtualHardDiskPath       = $configParams.folderPath.vhd
+    EnableEnhancedSessionMode = $true
+}
+Set-VMHost @params
+
+# Create a NAT vSwitch.
+$params = @{
+    Name        = $configParams.natOnLabHost.vSwitchName
+    SwitchType  = 'Internal'
+}
+New-VMSwitch @params
+
+# Assign an IP address to the NAT vSwitch network interface.
+$params= @{
+    InterfaceIndex = (Get-NetAdapter | Where-Object { $_.Name -match $configParams.natOnLabHost.vSwitchName }).ifIndex
+    AddressFamily  = 'IPv4'
+    IPAddress      = $configParams.natOnLabHost.hostIpAddress
+    PrefixLength   = $configParams.natOnLabHost.hostPrefixLength
+}
+New-NetIPAddress @params
+
+# Create a network NAT.
+$params = @{
+    Name                             = $configParams.natOnLabHost.vSwitchName
+    InternalIPInterfaceAddressPrefix = $configParams.natOnLabHost.subnet
+}
+New-NetNat @params
+
+Stop-Transcript
