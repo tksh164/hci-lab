14617cf099af797d8c54896cb687f85928a819c3
Takeshi Katano <takatano@outlook.com>
2022-12-30 19:26:28 +0900

    Update

--- customscripts/common.psm1
+++ customscripts/common.psm1
@@ -3,8 +3,46 @@ function GetConfigParameters
     [CmdletBinding()]
     param ()
 
-    $encodedUserData = Invoke-RestMethod -UseBasicParsing -Method Get -Headers @{ Metadata = 'true' } -Uri 'http://169.254.169.254/metadata/instance/compute/userData?api-version=2021-12-13&format=text'
+    $params = @{
+        Method  = 'Get'
+        Uri     = 'http://169.254.169.254/metadata/instance/compute/userData?api-version=2021-12-13&format=text'
+        Headers = @{
+            Metadata = 'true'
+        }
+        UseBasicParsing = $true
+    }
+    $encodedUserData = Invoke-RestMethod @params
     [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($encodedUserData)) | ConvertFrom-Json
+}
+
+function GetSecret
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $KeyVaultName,
+
+        [Parameter(Mandatory = $true)]
+        [string] $SecretName
+    )
+
+    $params = @{
+        Method  = 'Get'
+        Uri     = 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2021-12-13&resource=https%3A%2F%2Fvault.azure.net'
+        Headers = @{
+            Metadata = 'true'
+        }
+    }
+    $accessToken = (Invoke-RestMethod @params).access_token
+
+    $params = @{
+        Method  = 'Get'
+        Uri     = ('https://{0}.vault.azure.net/secrets/{1}?api-version=7.3' -f $KeyVaultName, $SecretName)
+        Headers = @{
+            Authorization = ('Bearer {0}' -f $accessToken)
+        }
+    }
+    (Invoke-RestMethod @params).value
 }
 
 function DownloadFile
