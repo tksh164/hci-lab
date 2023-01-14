8b50b824e89723c5108e848f7b51c9c4467370da
Takeshi Katano <takatano@outlook.com>
2023-01-15 03:07:52 +0900

    Fix logging

--- customscripts/configure-lab-host.ps1
+++ customscripts/configure-lab-host.ps1
@@ -101,7 +101,7 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\ServerManager' -Name 'DoNotPopW
 'Setting to hide the Network Location wizard. All networks will be Public.' | WriteLog -Context $env:ComputerName
 New-Item -ItemType Directory -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Network' -Name 'NewNetworkWindowOff'
 
-'Creating shortcut for Hyper-V Manager on the desktop....' | &$WriteLog -Context $vmName
+'Creating shortcut for Hyper-V Manager on the desktop....' | WriteLog -Context $env:ComputerName
 $wshShell = New-Object -ComObject 'WScript.Shell'
 $shortcut = $wshShell.CreateShortcut('C:\Users\Public\Desktop\Hyper-V Manager.lnk')
 $shortcut.TargetPath = '%windir%\System32\mmc.exe'
@@ -110,7 +110,7 @@ $shortcut.Description = 'Hyper-V Manager provides management access to your virt
 $shortcut.IconLocation = '%ProgramFiles%\Hyper-V\SnapInAbout.dll,0'
 $shortcut.Save()
 
-'Creating shortcut for Windows Admin Center VM on the desktop....' | &$WriteLog -Context $vmName
+'Creating shortcut for Windows Admin Center VM on the desktop....' | WriteLog -Context $env:ComputerName
 $wshShell = New-Object -ComObject 'WScript.Shell'
 $shortcut = $wshShell.CreateShortcut('C:\Users\Public\Desktop\Windows Admin Center VM.lnk')
 $shortcut.TargetPath = '%windir%\System32\mstsc.exe'
