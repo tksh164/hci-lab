a89cbfb6148903aaff556d919c1bb175a40f8249
Takeshi Katano <takatano@outlook.com>
2023-02-11 03:44:35 +0900

    Change ADDS DC VM base VHD to Server Core

--- customscripts/create-adds-dc-vm.ps1
+++ customscripts/create-adds-dc-vm.ps1
@@ -45,9 +45,10 @@ function WaitingForReadyToDC
 $vmName = $configParams.addsDC.vmName
 
 'Creating the OS disk for the VM...' | WriteLog -Context $vmName
+$imageIndex = 3  # Datacenter (Server Core)
 $params = @{
     Differencing = $true
-    ParentPath   = [IO.Path]::Combine($configParams.labHost.folderPath.vhd, (BuildBaseVhdFileName -OperatingSystem 'ws2022' -ImageIndex 4 -Culture $configParams.guestOS.culture))
+    ParentPath   = [IO.Path]::Combine($configParams.labHost.folderPath.vhd, (BuildBaseVhdFileName -OperatingSystem 'ws2022' -ImageIndex $imageIndex -Culture $configParams.guestOS.culture))
     Path         = [IO.Path]::Combine($configParams.labHost.folderPath.vm, $vmName, 'osdisk.vhdx')
 }
 $vmOSDiskVhd = New-VHD  @params
