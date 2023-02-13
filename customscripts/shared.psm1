cb8fece837530e1ae05e819ac154b12983be9137
Takeshi Katano <takatano@outlook.com>
2023-02-13 21:13:46 +0900

    Change log format

--- customscripts/shared.psm1
+++ customscripts/shared.psm1
@@ -13,7 +13,7 @@ function WriteLog
         [string] $Type = 'Verbose'
     )
 
-    $builtMessage = '[{0}] [{1}] {2}' -f [DateTime]::Now.ToString('yyyy-MM-dd HH:mm:ss'), $Context, $Message
+    $builtMessage = '[{0:yyyy-MM-dd HH:mm:ss}] [{1}] {2}' -f [DateTime]::Now, $Context, $Message
     switch ($Type) {
         'Warning' { Write-Warning -Message $builtMessage }
         'Error'   { Write-Error -Message $builtMessage }
