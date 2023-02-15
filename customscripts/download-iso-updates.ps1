4bddf5c0606e902abb0ab0e43fd6246743713b0c
Takeshi Katano <takatano@outlook.com>
2023-02-15 22:15:42 +0900

    Update information site URI for updates

--- customscripts/download-iso-updates.ps1
+++ customscripts/download-iso-updates.ps1
@@ -67,20 +67,20 @@ function DownloadUpdates
     # OS: https://learn.microsoft.com/en-us/azure-stack/hci/release-information
     #
     # Azure Stack HCI 22H2
-    # OS: https://support.microsoft.com/en-us/topic/fea63106-a0a9-4b6c-bb72-a07985c98a56
-    # .NET: https://support.microsoft.com/en-us/topic/57cf4b09-c538-4bdf-8954-37b690e52a12
+    # OS: https://support.microsoft.com/en-us/help/5018894
+    # .NET: https://support.microsoft.com/en-us/help/5022726
     #
     # Azure Stack HCI 21H2
-    # OS: https://support.microsoft.com/en-us/topic/5c5e6adf-e006-4a29-be22-f6faeff90173
-    # .NET: https://support.microsoft.com/en-us/topic/fde41bff-0ae6-479f-9e35-708f62ebbc08
+    # OS: https://support.microsoft.com/en-us/help/5004047
+    # .NET: https://support.microsoft.com/en-us/help/5023809
     #
     # Azure Stack HCI 20H2
-    # OS: https://support.microsoft.com/en-us/topic/64c79b7f-d536-015d-b8dd-575f01090efd
+    # OS: https://support.microsoft.com/en-us/help/4595086
     # .NET: 
     #
     # Windows Server 2022
-    # OS: https://support.microsoft.com/en-us/topic/e1caa597-00c5-4ab9-9f3e-8212fe80b2ee
-    # .NET: https://support.microsoft.com/en-us/topic/f61ae6ae-6f7a-493f-84d3-42ae1ebff494
+    # OS: https://support.microsoft.com/en-us/help/5005454
+    # .NET: https://support.microsoft.com/en-us/help/5006918
     #
 
     $updates = @{
