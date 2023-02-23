1cf5a447661476f4510001dd110bf50f1185e169
Takeshi Katano <takatano@outlook.com>
2023-02-23 16:53:58 +0900

    Add Force option

--- customscripts/create-wac-vm.ps1
+++ customscripts/create-wac-vm.ps1
@@ -154,7 +154,7 @@ Invoke-Command @params -ScriptBlock {
     Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\ServerManager' -Name 'DoNotPopWACConsoleAtSMLaunch' -Value 1
 
     'Hide the Network Location wizard. All networks will be Public.' | &$WriteLog -Context $vmName
-    New-Item -ItemType Directory -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Network' -Name 'NewNetworkWindowOff'
+    New-Item -ItemType Directory -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Network' -Name 'NewNetworkWindowOff' -Force
 
     'Renaming the network adapters...' | &$WriteLog -Context $vmName
     Get-NetAdapterAdvancedProperty -RegistryKeyword 'HyperVNetworkAdapterName' | ForEach-Object -Process {
