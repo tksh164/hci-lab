07f93abc589483487f855afac9b837ab43c21ada
Takeshi Katano <takatano@outlook.com>
2023-05-02 23:19:52 +0900

    Change VHD creation order
    Create WS GUI VHD first, then create WS Server Core VHD.

--- customscripts/create-base-vhd.ps1
+++ customscripts/create-base-vhd.ps1
@@ -30,7 +30,7 @@ $convertWimScriptFile
 # NOTE: Only one VHD file can create from the same single ISO file. The second VHD creation will fail if create
 # multiple VHDs from the same single ISO because the ISO unmount when finish first one.
 'Copying Windows Server ISO file...' | WriteLog -Context $env:ComputerName
-$tempIsoFileNameSuffix = 'temp'
+$tempIsoFileNameSuffix = 'concurrent'
 $ws2022SourceIsoFilePath = [IO.Path]::Combine($labConfig.labHost.folderPath.temp, (GetIsoFileName -OperatingSystem 'ws2022' -Culture $labConfig.guestOS.culture))
 $ws2022TempIsoFilePath = [IO.Path]::Combine($labConfig.labHost.folderPath.temp, (GetIsoFileName -OperatingSystem 'ws2022' -Culture $labConfig.guestOS.culture -Suffix $tempIsoFileNameSuffix))
 Copy-Item -LiteralPath $ws2022SourceIsoFilePath -Destination $ws2022TempIsoFilePath -Force -PassThru
@@ -54,6 +54,24 @@ $params = [PSCustomObject] @{
 }
 $jobs += Start-Job -Name 'HCI node' -LiteralPath $jobScriptFilePath -InputObject $params
 
+if (-not (($labConfig.hciNode.operatingSystem.sku -eq 'ws2022') -and ($labConfig.hciNode.operatingSystem.imageIndex -eq 4))) {
+    # Use the Windows Server with Desktop Experience VHD for Windows Admin Center always.
+    'Starting the Windows Server Desktop Experience base VHD creation job.' | WriteLog -Context $env:ComputerName
+    $params = [PSCustomObject] @{
+        PSModuleNameToImport = (Get-Module -Name 'shared').Path, $convertWimScriptFile.FullName
+        IsoFolder            = $labConfig.labHost.folderPath.temp
+        OperatingSystem      = 'ws2022'
+        ImageIndex           = 4  # Datacenter with Desktop Experience
+        Culture              = $labConfig.guestOS.culture
+        VhdFolder            = $labConfig.labHost.folderPath.vhd
+        UpdatesFolder        = $labConfig.labHost.folderPath.updates
+        WorkFolder           = $labConfig.labHost.folderPath.temp
+        LogFolder            = $labConfig.labHost.folderPath.log
+        LogFileName          = 'create-base-vhd-job-wsgui'
+    }
+    $jobs += Start-Job -Name 'WS Desktop Experience' -LiteralPath $jobScriptFilePath -InputObject $params
+}
+
 if (-not (($labConfig.hciNode.operatingSystem.sku -eq 'ws2022') -and ($labConfig.hciNode.operatingSystem.imageIndex -eq 3))) {
     # Use the Windows Server Server Core VHD for AD DS domain controller always.
     'Starting the Windows Server Core base VHD creation job.' | WriteLog -Context $env:ComputerName
@@ -71,24 +89,6 @@ if (-not (($labConfig.hciNode.operatingSystem.sku -eq 'ws2022') -and ($labConfig
         LogFileName          = 'create-base-vhd-job-wscore'
     }
     $jobs += Start-Job -Name 'WS Server Core' -LiteralPath $jobScriptFilePath -InputObject $params
-}
-
-if (-not (($labConfig.hciNode.operatingSystem.sku -eq 'ws2022') -and ($labConfig.hciNode.operatingSystem.imageIndex -eq 4))) {
-    # Use the Windows Server with Desktop Experience VHD for Windows Admin Center always.
-    'Starting the Windows Server Desktop Experience base VHD creation job.' | WriteLog -Context $env:ComputerName
-    $params = [PSCustomObject] @{
-        PSModuleNameToImport = (Get-Module -Name 'shared').Path, $convertWimScriptFile.FullName
-        IsoFolder            = $labConfig.labHost.folderPath.temp
-        OperatingSystem      = 'ws2022'
-        ImageIndex           = 4  # Datacenter with Desktop Experience
-        Culture              = $labConfig.guestOS.culture
-        VhdFolder            = $labConfig.labHost.folderPath.vhd
-        UpdatesFolder        = $labConfig.labHost.folderPath.updates
-        WorkFolder           = $labConfig.labHost.folderPath.temp
-        LogFolder            = $labConfig.labHost.folderPath.log
-        LogFileName          = 'create-base-vhd-job-wsgui'
-    }
-    $jobs += Start-Job -Name 'WS Desktop Experience' -LiteralPath $jobScriptFilePath -InputObject $params
 }
 
 $jobs | Format-Table -Property Id, Name, State, HasMoreData, PSBeginTime, PSEndTime
