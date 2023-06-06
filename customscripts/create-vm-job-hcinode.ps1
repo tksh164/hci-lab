851c459f0b610b19ac5ab7f8b7de532e2ea8c120
Takeshi Katano <takatano@outlook.com>
2023-06-07 00:44:14 +0900

    Optimize installing Windows features beased on HCI node OS SKU

--- customscripts/create-vm-job-hcinode.ps1
+++ customscripts/create-vm-job-hcinode.ps1
@@ -38,6 +38,34 @@ function Invoke-HciNodeRamSizeCalculation
     return [Math]::Floor((($totalRamBytes - $labHostReservedRamBytes - $AddsDcVMRamBytes - $WacVMRamBytes) / $NodeCount) / 2MB) * 2MB
 }
 
+function Get-WindowsFeatureToInstall
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $HciNodeOperatingSystemSku
+    )
+
+    $featureNames = @(
+        'Hyper-V',  # Note: https://twitter.com/pronichkin/status/1294308601276719104
+        'Failover-Clustering',
+        'FS-FileServer',
+        'Data-Center-Bridging',
+        'RSAT-AD-PowerShell',
+        'RSAT-Hyper-V-Tools',
+        'RSAT-Clustering'
+    )
+    if ($C_AzureStackHciOperatingSystemSkus -contains $HciNodeOperatingSystemSku) {
+        $featureNames += 'FS-Data-Deduplication'
+        $featureNames += 'BitLocker'
+    
+        if ($HciNodeOperatingSystemSku -ne $C_OperatingSystemSku.AzureStackHci20H2) {
+            $featureNames += 'NetworkATC'
+        }
+    }
+    return $featureNames
+}
+
 $ErrorActionPreference = [Management.Automation.ActionPreference]::Stop
 $WarningPreference = [Management.Automation.ActionPreference]::Continue
 $VerbosePreference = [Management.Automation.ActionPreference]::Continue
@@ -241,15 +269,7 @@ Set-UnattendAnswerFileToVhd @params
 'Installing the roles and features to the VHD...' | Write-ScriptLog -Context $nodeConfig.VMName
 $params = @{
     VhdPath     = $vmOSDiskVhd.Path
-    FeatureName = @(
-        'Hyper-V',  # Note: https://twitter.com/pronichkin/status/1294308601276719104
-        'Failover-Clustering',
-        'FS-FileServer',
-        'Data-Center-Bridging',  # Needs for WS2022 clsuter by WAC
-        'RSAT-Hyper-V-Tools',
-        'RSAT-Clustering',
-        'RSAT-AD-PowerShell'  # Needs for WS2022 clsuter by WAC
-    )
+    FeatureName = Get-WindowsFeatureToInstall -HciNodeOperatingSystemSku $labConfig.hciNode.operatingSystem.sku
     LogFolder   = $labConfig.labHost.folderPath.log
 }
 Install-WindowsFeatureToVhd @params
