be89a2d36ab5bf0c24075588397b6b98184f4b95
Takeshi Katano <takatano@outlook.com>
2024-07-07 04:22:51 +0900

    Change code order

--- template/customscripts/create-base-vhd-job.ps1
+++ template/customscripts/create-base-vhd-job.ps1
@@ -173,9 +173,9 @@ try {
     'Initialize the VHD completed.' | Write-ScriptLog
 
     # The partition type GUIDs.
-    $PARTITION_BASIC_DATA_GUID = '{ebd0a0a2-b9e5-4433-87c0-68b6b72699c7}'
     $PARTITION_SYSTEM_GUID = '{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}'
     $PARTITION_MSFT_RESERVED_GUID = '{e3c9e316-0b5c-4db8-817d-f92df00215ae}'
+    $PARTITION_BASIC_DATA_GUID = '{ebd0a0a2-b9e5-4433-87c0-68b6b72699c7}'
 
     'Create an EFI system partition.' | Write-ScriptLog
     $systemPartition = $disk | New-Partition -GptType $PARTITION_SYSTEM_GUID -Size 200MB
