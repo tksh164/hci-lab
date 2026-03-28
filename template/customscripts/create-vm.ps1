8b5d7f731a4b99177248409332fbd72fb3ae1813
Takeshi Katano <takatano@outlook.com>
2026-03-29 02:32:15 +0900

    Rework entire script

--- template/customscripts/create-vm.ps1
+++ template/customscripts/create-vm.ps1
@@ -7,59 +7,72 @@ $VerbosePreference = [Management.Automation.ActionPreference]::Continue
 $ProgressPreference = [Management.Automation.ActionPreference]::SilentlyContinue
 
 try {
+    # Mandatory pre-processing.
     $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-
-    Import-Module -Name ([IO.Path]::Combine($PSScriptRoot, 'common.psm1')) -Force
-
+    Import-Module -Name ([System.IO.Path]::Combine($PSScriptRoot, 'common.psm1')) -Force
     $labConfig = Get-LabDeploymentConfig
     Start-ScriptLogging -OutputDirectory $labConfig.labHost.folderPath.log
+    'Script path: "{0}"' -f $PSCommandPath | Write-ScriptLog
+    'Lab deployment config: {0}' -f ($labConfig | ConvertTo-Json -Depth 16) | Write-ScriptLog
 
-    'Script file: {0}' -f $PSScriptRoot | Write-ScriptLog
-    'Lab deployment config:' | Write-ScriptLog
-    $labConfig | ConvertTo-Json -Depth 16 | Write-Host
-
-    $jobs = @()
-
-    'Start the AD DS VM creation job.' | Write-ScriptLog
-    $jobScriptFilePath = [IO.Path]::Combine($PSScriptRoot, 'create-vm-job-addsdc.ps1')
-    $params = @{
-        PSModuleNameToImport = (Get-Module -Name 'common').Path
-        LogFileName          = [IO.Path]::GetFileNameWithoutExtension($jobScriptFilePath)
-    }
-    $jobs += Start-Job -Name 'addsdc-vm' -LiteralPath $jobScriptFilePath -InputObject ([PSCustomObject] $params)
-    'Start the AD DS VM creation job completed.' | Write-ScriptLog
-
-    'Start the management server VM creation job.' | Write-ScriptLog
-    $jobScriptFilePath = [IO.Path]::Combine($PSScriptRoot, 'create-vm-job-wac.ps1')
-    $params = @{
-        PSModuleNameToImport = (Get-Module -Name 'common').Path
-        LogFileName          = [IO.Path]::GetFileNameWithoutExtension($jobScriptFilePath)
-    }
-    $jobs += Start-Job -Name 'wac-vm' -LiteralPath $jobScriptFilePath -InputObject ([PSCustomObject] $params)
-    'Start the management server VM creation job completed.' | Write-ScriptLog
-
-    'Start the HCI node VM creation jobs.' | Write-ScriptLog
-    $jobScriptFilePath = [IO.Path]::Combine($PSScriptRoot, 'create-vm-job-hcinode.ps1')
-    for ($nodeIndex = 0; $nodeIndex -lt $labConfig.hciNode.nodeCount; $nodeIndex++) {
-        $vmName = Format-HciNodeName -Format $labConfig.hciNode.vmName -Offset $labConfig.hciNode.vmNameOffset -Index $nodeIndex
-        'Start a VM creation job for the VM "{0}".' -f $vmName | Write-ScriptLog
-        $params = @{
-            NodeIndex            = $nodeIndex
-            PSModuleNameToImport = (Get-Module -Name 'common').Path
-            LogFileName          = [IO.Path]::GetFileNameWithoutExtension($jobScriptFilePath) + '-' + $vmName
+    'Prepare the job specs.' | Write-ScriptLog
+    $importModulePaths = @( (Get-Module -Name 'common').Path )
+    $jobSpecs = @(
+        # AD DS DC
+        [PSCustomObject]@{
+            JobName           = 'addsdc'
+            JobScriptFilePath = [System.IO.Path]::Combine($PSScriptRoot, 'create-vm-job-addsdc.ps1')
+            ImportModulePaths = $importModulePaths
+            LogFileName       = 'create-vm-job-addsdc'
+            LogContext        = $labConfig.addsDC.vmName
+        },
+        # Workbox
+        [PSCustomObject]@{
+            JobName           = 'workbox'
+            JobScriptFilePath = [System.IO.Path]::Combine($PSScriptRoot, 'create-vm-job-wac.ps1')
+            ImportModulePaths = $importModulePaths
+            LogFileName       = 'create-vm-job-wac'
+            LogContext        = $labConfig.wac.vmName
         }
-        $jobs += Start-Job -Name $vmName -LiteralPath $jobScriptFilePath -InputObject ([PSCustomObject] $params)
+    )
+    # HCI nodes
+    $jobScriptFilePath = [System.IO.Path]::Combine($PSScriptRoot, 'create-vm-job-hcinode.ps1')
+    $jobSpecs += for ($nodeIndex = 0; $nodeIndex -lt $labConfig.hciNode.nodeCount; $nodeIndex++) {
+        $nodeName = Format-HciNodeName -Format $labConfig.hciNode.vmName -Offset $labConfig.hciNode.vmNameOffset -Index $nodeIndex
+        [PSCustomObject]@{
+            JobName           = $nodeName
+            JobScriptFilePath = $jobScriptFilePath
+            ImportModulePaths = $importModulePaths
+            LogFileName       = [System.IO.Path]::GetFileNameWithoutExtension($jobScriptFilePath) + '-' + $nodeName
+            LogContext        = $nodeName
+            JobParamsJson     = (@{
+                NodeIndex = $nodeIndex
+            } | ConvertTo-Json -Compress -Depth 5)
+        }
     }
-    'Start the HCI node VM creation jobs completed.' | Write-ScriptLog
+    'The job specs: {0}' -f ($jobSpecs | Format-List -Property '*' | Out-String -Width 200) | Write-ScriptLog
+    'Prepare the job specs has been completed.' | Write-ScriptLog
 
-    'The HCI lab VMs creation job status:' | Write-ScriptLog
-    $jobs | Format-Table -Property Id, Name, State, HasMoreData, PSBeginTime, PSEndTime
+    'Create the Hyper-V VM creation jobs.' | Write-ScriptLog
+    $jobs = @()
+    $jobs += foreach ($spec in $jobSpecs) {
+        $jobParams = @{
+            ImportModulePath = $spec.ImportModulePaths
+            LogFileName      = $spec.LogFileName
+            LogContext       = $spec.LogContext
+            JobParamsJson    = $spec.JobParamsJson
+        }
+        Start-Job -Name $spec.JobName -LiteralPath $spec.JobScriptFilePath -InputObject ([PSCustomObject] $jobParams)
+        'The job "{0}" has started.' -f $spec.JobName | Write-ScriptLog
+    }
+    $jobStatus = $jobs | Format-Table -Property 'Id', 'Name', 'State', 'HasMoreData', 'PSBeginTime', 'PSEndTime' | Out-String -Width 200
+    'The Hyper-V VM creation job status: {0}' -f $jobStatus | Write-ScriptLog
+    'Create the Hyper-V VM creation jobs has been completed.' | Write-ScriptLog
 
-    'Start waiting for all HCI lab VMs creation jobs completion.' | Write-ScriptLog
+    'Waiting for completion of the all Hyper-V VM creation jobs.' | Write-ScriptLog
     $jobs | Receive-Job -Wait
-    'All HCI lab VMs creation jobs completed.' | Write-ScriptLog
 
-    'The HCI lab VMs creation has been successfully completed.' | Write-ScriptLog
+    'This script has been completed all tasks.' | Write-ScriptLog
 }
 catch {
     $exceptionMessage = New-ExceptionMessage -ErrorRecord $_
@@ -67,11 +80,11 @@ catch {
     throw $exceptionMessage
 }
 finally {
-    'The HCI lab VMs creation job final status:' | Write-ScriptLog
-    $jobs | Format-Table -Property Id, Name, State, HasMoreData, PSBeginTime, PSEndTime
+    $finalStatus = $jobs | Format-Table -Property 'Id', 'Name', 'State', 'HasMoreData', 'PSBeginTime', 'PSEndTime', @{ Label = 'ElapsedTime'; Expression = { $_.PSEndTime - $_.PSBeginTime } } | Out-String -Width 200
+    'The final status of the Hyper-V VM creation jobs: {0}' -f $finalStatus | Write-ScriptLog
 
-    'The HCI lab VMs creation has been finished.' | Write-ScriptLog
+    # Mandatory post-processing.
     $stopWatch.Stop()
-    'Duration of this script ran: {0}' -f $stopWatch.Elapsed.toString('hh\:mm\:ss') | Write-ScriptLog
+    'Script duration: {0}' -f $stopWatch.Elapsed.toString('hh\:mm\:ss') | Write-ScriptLog
     Stop-ScriptLogging
 }
