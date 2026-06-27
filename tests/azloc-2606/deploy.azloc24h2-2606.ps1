223250238d6226143f944ba619e3c726afde425a
Takeshi Katano <takatano@outlook.com>
2026-06-28 02:49:17 +0900

    Update deploy script for Azure Local 2606

--- tests/azloc-2606/deploy.azloc24h2-2606.ps1
+++ tests/azloc-2606/deploy.azloc24h2-2606.ps1
@@ -1,18 +1,27 @@
 [CmdletBinding()]
-param ()
+param (
+    [Parameter(Mandatory = $false, HelpMessage = 'Resource group location.')]
+    [string] $Location = 'japaneast',
+
+    [Parameter(Mandatory = $false, HelpMessage = 'Resource group tags.')]
+    [HashTable] $Tag = @{ 'usage' = 'experimental' },
+
+    [switch] $WhatIf,
+
+    [switch] $ValidateOnly
+)
 
 $symbol = 'azloc24h2-2606'
 
 $params = @{
-    ResourceGroupName      = 'labenv-{0}-{1}-test' -f $symbol, [datetime]::Now.ToString('yyMMddHHmmss')
-    ResourceGroupLocation  = 'japaneast'
+    ResourceGroupName      = 'labenv-{0}-{1}' -f $symbol, [datetime]::Now.ToString('yyMMddHHmmss')
+    ResourceGroupLocation  = $Location
     TemplateFile           = Resolve-Path -LiteralPath '../../templates/labenv/template.json' -RelativeBasePath $PSScriptRoot
     TemplateParametersFile = Resolve-Path -LiteralPath ('./{0}.parameters.json' -f $symbol) -RelativeBasePath $PSScriptRoot
-    ResourceGroupTag       = @{ 'usage' = 'experimental' }
-    WhatIf                 = $false
-    ValidateOnly           = $false
-} 
-
+    ResourceGroupTag       = $Tag
+    WhatIf                 = $WhatIf
+    ValidateOnly           = $ValidateOnly
+}
 $testDeployScriptPath = Resolve-Path -LiteralPath '../testdeploy.ps1' -RelativeBasePath $PSScriptRoot
 
 & $testDeployScriptPath @params
