93c60436dc6ba5e2a63cb839c711b090885e3b8c
Takeshi Katano <takatano@outlook.com>
2026-06-28 02:38:30 +0900

    Change resource group name

--- tests/azloc-2606/deploy.azloc24h2-2606.ps1
+++ tests/azloc-2606/deploy.azloc24h2-2606.ps1
@@ -4,7 +4,7 @@ param ()
 $symbol = 'azloc24h2-2606'
 
 $params = @{
-    ResourceGroupName      = '{0}-{1}-test' -f $symbol, [datetime]::Now.ToString('yyMMddHHmmss')
+    ResourceGroupName      = 'labenv-{0}-{1}-test' -f $symbol, [datetime]::Now.ToString('yyMMddHHmmss')
     ResourceGroupLocation  = 'japaneast'
     TemplateFile           = Resolve-Path -LiteralPath '../../templates/labenv/template.json' -RelativeBasePath $PSScriptRoot
     TemplateParametersFile = Resolve-Path -LiteralPath ('./{0}.parameters.json' -f $symbol) -RelativeBasePath $PSScriptRoot
