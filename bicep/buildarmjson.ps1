c1c7216c12a6690523083016a7a9a583308fc92d
Takeshi Katano <takatano@outlook.com>
2026-06-28 01:23:44 +0900

    Update Bicep build script

--- bicep/buildarmjson.ps1
+++ bicep/buildarmjson.ps1
@@ -1,17 +1,17 @@
 #Requires -Version 7
 
-$bicepFilePaths = @(
-    './labenv/bastion.bicep',
-    './labenv/cloudwitness.bicep',
-    './labenv/customscript.bicep',
-    './labenv/dsc.bicep',
-    './labenv/hostvm.bicep',
-    './labenv/keyvault-rbac.bicep',
-    './labenv/keyvault.bicep',
-    './labenv/labenv.bicep',
-    './labenv/vnet.bicep'
+$outputFolderBasePath = '../templates'
+$buildConfig = @(
+    @{ BicepFilePath = './labenv/labenv.bicep';        OutputFolderBasePath = $outputFolderBasePath; JsonFileName = 'template.json' },
+    @{ BicepFilePath = './labenv/bastion.bicep';       OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null },
+    @{ BicepFilePath = './labenv/cloudwitness.bicep';  OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null },
+    @{ BicepFilePath = './labenv/customscript.bicep';  OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null },
+    @{ BicepFilePath = './labenv/dsc.bicep';           OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null },
+    @{ BicepFilePath = './labenv/hostvm.bicep';        OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null },
+    @{ BicepFilePath = './labenv/keyvault-rbac.bicep'; OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null },
+    @{ BicepFilePath = './labenv/keyvault.bicep';      OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null },
+    @{ BicepFilePath = './labenv/vnet.bicep';          OutputFolderBasePath = $outputFolderBasePath; JsonFileName = $null }
 )
-$outputBaseFolderPath = '../templates'
 
 # Check the az command.
 $azCommand = Get-Command -Name 'az'
@@ -19,17 +19,20 @@ Write-Host 'az: ' -NoNewline -ForegroundColor Cyan
 Write-Host ('"{0}"' -f $azCommand.Path)
 
 # Build the bicep file to ARM template file.
-foreach ($bicepFilePath in $bicepFilePaths) {
-    $bicepFileFullPath = (Resolve-Path -LiteralPath $bicepFilePath -RelativeBasePath $PSScriptRoot).Path
-
-    $replacePart = [IO.Path]::GetDirectoryName([IO.Path]::GetDirectoryName($bicepFileFullPath))
-    $outputBaseFolderFullPath = (Resolve-Path -LiteralPath $outputBaseFolderPath -RelativeBasePath $PSScriptRoot).Path
-    $outputFolderFullPath = [IO.Path]::GetDirectoryName($bicepFileFullPath.Replace($replacePart, $outputBaseFolderFullPath))
+foreach ($config in $buildConfig) {
+    $bicepFileFullPath = (Resolve-Path -LiteralPath $config.BicepFilePath -RelativeBasePath $PSScriptRoot).Path
+    $jsonFileRelativePath = if ($config.JsonFileName -ne $null) {
+        [IO.Path]::Combine([IO.Path]::GetDirectoryName($config.BicepFilePath), $config.JsonFileName)
+    }
+    else {
+        $config.BicepFilePath.Replace('.bicep', '.json')
+    }
+    $jsonFileFullPath = [IO.Path]::GetFullPath([IO.Path]::Combine($PSScriptRoot, $config.OutputFolderBasePath, $jsonFileRelativePath))
 
     Write-Host 'Build: ' -NoNewline -ForegroundColor Cyan
     Write-Host ('"{0}"' -f $bicepFileFullPath) -NoNewline
     Write-Host ' -> ' -NoNewline -ForegroundColor Cyan
-    Write-Host ('"{0}"' -f $outputFolderFullPath)
+    Write-Host ('"{0}"' -f $jsonFileFullPath)
 
-    & $azCommand.Path @('bicep', 'build', '--outdir', $outputFolderFullPath, '--file', $bicepFileFullPath)
+    & $azCommand.Path @('bicep', 'build', '--outfile', $jsonFileFullPath, '--file', $bicepFileFullPath)
 }
